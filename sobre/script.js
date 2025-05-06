6d947787a2c795d420fe30abf0cd0823a0a0799c
Luis Antônio Gambogi <netogambogi@hotmail.com>
2025-05-06 11:14:30 -0300

    navbar responsiva

---
+++ sobre/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.barra-menu');
+  const botaoContato = document.querySelector('.botao-contato');
+  const toggleIcon = toggleButton.querySelector('img');
+
+  toggleButton.addEventListener('click', function () {
+    const isMenuActive = menu.classList.toggle('ativo');
+    botaoContato.classList.toggle('ativo');
+
+    if (isMenuActive) {
+      toggleIcon.src = 'img.sobre/fechar.png';
+      toggleIcon.alt = 'Fechar menu';
+    } else {
+      toggleIcon.src = 'img.sobre/menu.png';
+      toggleIcon.alt = 'Menu';
+    }
+  });
+});
