ea465b35700724edc788b545b67bb9807fb503cb
Luis Antônio Gambogi <netogambogi@hotmail.com>
2025-05-06 12:38:27 -0300

    troca de diretorio

--- pagprodutos/script.js
+++ pagprodutos/script.js
@@ -9,10 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoContato.classList.toggle('ativo');
 
     if (isMenuActive) {
-      toggleIcon.src = 'img.produtos/fechar.png';
+      toggleIcon.src = 'img/fechar.png';
       toggleIcon.alt = 'Fechar menu';
     } else {
-      toggleIcon.src = 'img.produtos/menu.png';
+      toggleIcon.src = 'img/menu.png';
       toggleIcon.alt = 'Menu';
     }
   });
